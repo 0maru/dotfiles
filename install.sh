30b6516cce8db538288ead7514b0fd152f1514fb
0maru <43493921+0maru@users.noreply.github.com>
2024-08-03 14:10:56 +0900

    clone済みの場合pullできるようにした

--- install.sh
+++ install.sh
@@ -5,7 +5,7 @@ INSTALL_DIR="$HOME/workspaces/github.com/0maru/dotfiles"
 
 if [ -d "$INSTALL_DIR" ]; then
   echo -e "\033[32mUpdating dotfiles...\033[m"
-#  git -C "$INSTALL_DIR" pull
+  git -C "$INSTALL_DIR" pull
 else
   echo -e "\033[32mInstalling dotfiles...\033[m"
   git clone https://github.com/0maru/dotfiles "$INSTALL_DIR"
