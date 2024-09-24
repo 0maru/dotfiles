927d2735cee5e779e169e058963f5f9d001da879
0maru <0maru.dev@gmail.com>
2024-09-24 11:10:53 +0900

    fix: true to TRUE

--- scripts/setup-defaults.sh
+++ scripts/setup-defaults.sh
@@ -7,9 +7,9 @@ fi
 # スクリーンショットの保存先を ~/Screenshots フォルダに変更する
 defaults write com.apple.screencapture location ~/Screenshots
 # dock は自動で隠す
-defaults write com.apple.dock autohide -bool true
+defaults write com.apple.dock autohide -bool TRUE
 # ネットワークディスクで.DS_Store ファイルを作成しない
-defaults write com.apple.desktopservices DSDontWriteNetworkStores -bool true
+defaults write com.apple.desktopservices DSDontWriteNetworkStores -bool TREU
 
 killall Dock
 killall Finder
