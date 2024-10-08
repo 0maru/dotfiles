93bc96819bd0a1758151d6fd18944cc637ec8400
0maru <0maru.dev@gmail.com>
2024-10-08 11:12:04 +0900

    ちらつき修正/色を変更

--- config/wezterm/wezterm.lua
+++ config/wezterm/wezterm.lua
@@ -14,26 +14,20 @@ config.color_scheme = 'GitHub Dark'
 config.window_background_opacity = 0.9
 config.show_new_tab_button_in_tab_bar = false
 config.hide_tab_bar_if_only_one_tab = true
+config.show_close_tab_button_in_tabs = false
 config.initial_rows = 60
 config.initial_cols = 140
 config.window_decorations = 'RESIZE'
 config.tab_bar_at_bottom = true
 config.window_frame = {
-    inactive_titlebar_bg = "none",
-    active_titlebar_bg = "none"
-}
-
-config.window_background_gradient = {
-    colors = {'#000000'}
+  inactive_titlebar_bg = "none",
+  active_titlebar_bg = "none"
 }
 config.colors = {
-    tab_bar = {
-        inactive_tab_edge = "none"
-    }
+  tab_bar = {
+    inactive_tab_edge = "none"
+  }
 }
-
--- config.show_close_tab_button_in_tabs = false
-
 
 -- keybindings
 config.disable_default_key_bindings = true
@@ -44,29 +38,32 @@ config.leader = {
   timeout_milliseconds = 1000
 }
 
+local SOLID_LEFT_ARROW = wezterm.nerdfonts.ple_lower_right_triangle
+local SOLID_RIGHT_ARROW = wezterm.nerdfonts.ple_upper_left_triangle
 wezterm.on("format-tab-title", function(tab, tabs, panes, config, hover, max_width)
-    local background = "#5c6d71"
-    local foreground = "#FFFFFF"
+  local background = "#FFDBED"
+  local foreground = "#333333"
+  local edge_background = "none"
 
-    if tab.is_active then
-        background = "#ae8b2d"
-        foreground = "#FFFFFF"
-    end
+  if tab.is_active then
+    background = "#0067C0"
+    foreground = "#E0FFFF"
+  end
 
-    local title = "   " .. wezterm.truncate_right(tab.active_pane.title, max_width - 1) .. "   "
+  local edge_foreground = background
+  local title = "   " .. wezterm.truncate_right(tab.active_pane.title, max_width - 1) .. "   "
 
-    return {{
-        Background = {
-            Color = background
-        }
-    }, {
-        Foreground = {
-            Color = foreground
-        }
-    }, {
-        Text = title
-    }}
+  return {
+    { Background = { Color = edge_background } },
+    { Foreground = { Color = edge_foreground } },
+    { Text = SOLID_LEFT_ARROW },
+    { Background = { Color = background } },
+    { Foreground = { Color = foreground } },
+    { Text = title },
+    { Background = { Color = edge_background } },
+    { Foreground = { Color = edge_foreground } },
+    { Text = SOLID_RIGHT_ARROW },
+  }
 end)
-
 
 return config
