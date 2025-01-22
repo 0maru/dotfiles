edb320a59d75ec4812e44ace41f29bc10388e8b4
0maru <43493921+0maru@users.noreply.github.com>
2025-01-22 23:23:32 +0900

    nvim プラグイン以外の設定を監理

---
+++ config/nvim/init.lua
@@ -0,0 +1,14 @@
+vim.opt.number = true
+vim.opt.expandtab = true
+vim.opt.tabstop = 2
+vim.opt.shiftwidth = 2
+vim.opt.smartindent = true
+vim.opt.termguicolors = true
+vim.opt.cursorline = true
+vim.opt.mouse = 'a'
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
+
+vim.g.mapleader = ' '
+vim.keymap.set('n', '<Leader>w', ':w<CR>')
+vim.keymap.set('n', '<Leader>q', ':q<CR>')
