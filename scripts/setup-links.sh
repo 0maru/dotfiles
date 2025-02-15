ad373f328b511ae8c1d2b8f9d8f4bb2242a283ac
0maru <43493921+0maru@users.noreply.github.com>
2025-02-15 12:00:37 +0900

    rename code to vscode

--- scripts/setup-links.sh
+++ scripts/setup-links.sh
@@ -11,3 +11,8 @@ ln -sfv "$REPO_DIR/config/"* "$XDG_CONFIG_HOME"
 ln -sfv "$XDG_CONFIG_HOME/zsh/.zshenv" "$HOME/.zshenv"
 ln -sfv "$XDG_CONFIG_HOME/ideavim/.ideavimrc" "$HOME/.ideavimrc"
 
+# VS Code, Cursor
+ln -sfv "$XDG_CONFIG_HOME/vscode/keybindings.json" "$HOME/Library/Application Support/Cursor/User/keybindings.json"
+ln -sfv "$XDG_CONFIG_HOME/vscode/settings.json" "$HOME/Library/Application Support/Cursor/User/settings.json"
+ln -sfv "$XDG_CONFIG_HOME/vscode/settings.json" "$HOME/Library/Application Support/Cursor/User/keybindings.json"
+ln -sfv "$XDG_CONFIG_HOME/vscode/settings.json" "$HOME/Library/Application Support/Cursor/User/settings.json"
