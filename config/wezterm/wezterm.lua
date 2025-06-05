6c3c4481e0488fb9b844b427cb2af0ed75b1862b
0maru <0maru.dev@gmail.com>
2025-06-05 22:56:39 +0900

    余分なスペースを削除

--- config/wezterm/wezterm.lua
+++ config/wezterm/wezterm.lua
@@ -72,7 +72,7 @@ wezterm.on(
     end
     local edge_foreground = background
     local title = tab_name(tab)
-  
+
     return {
       { Background = { Color = edge_background } },
       { Foreground = { Color = edge_foreground } },
@@ -86,6 +86,6 @@ wezterm.on(
     }
   end
 )
-  
+
 
 return config
