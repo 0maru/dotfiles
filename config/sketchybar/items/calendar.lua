e46c0430eadd8c35c38c3610313aa3349ca8abeb
0maru <0maru.dev@gmail.com>
2026-01-28 09:43:09 +0900

    日時の表記を日本に合わせた

--- config/sketchybar/items/calendar.lua
+++ config/sketchybar/items/calendar.lua
@@ -1,105 +1,34 @@
 local settings = require("settings")
 local colors = require("colors")
 
--- Padding item required because of bracket
--- sbar.add("item", { position = "right", width = settings.group_paddings })
-local cal_clock = sbar.add("item", {
-	icon = {
-		drawing = "off",
-	},
+-- Japanese day of week
+local day_of_week_ja = { "日", "月", "火", "水", "木", "金", "土" }
+
+local cal = sbar.add("item", "widgets.calendar", {
+	icon = { drawing = false },
 	label = {
 		color = colors.tn_blue,
-		padding_right = 0,
-		align = "right",
 		font = { family = settings.font.numbers },
-		y_offset = 6,
+		padding_left = 10,
+		padding_right = 10,
 	},
 	position = "right",
 	update_freq = 1,
-	padding_left = -43,
-	padding_right = 12,
 })
 
-local cal_day_of_week = sbar.add("item", {
-	icon = {
-		drawing = "off",
-	},
-	label = {
-		color = colors.tn_blue,
-		padding_right = 0,
-		align = "center",
-		font = { family = settings.font.numbers },
-		y_offset = -6,
-	},
-	position = "right",
-	update_freq = 1,
-	padding_left = 0,
-	padding_right = 0,
-})
-
-local cal_month = sbar.add("item", {
-	icon = {
-		drawing = "off",
-	},
-	label = {
-		color = colors.tn_blue,
-		padding_right = 0,
-		align = "center",
-		font = { family = settings.font.numbers },
-		y_offset = 6,
-		padding_left = 0,
-	},
-	position = "right",
-	update_freq = 1,
-	padding_left = -24,
-	padding_right = 5,
-})
-
-local cal_day = sbar.add("item", {
-	icon = {
-		drawing = "off",
-	},
-	label = {
-		color = colors.tn_blue,
-		padding_right = 0,
-		align = "center",
-		font = { family = settings.font.numbers },
-		y_offset = -6,
-	},
-	width = 32,
-	position = "right",
-	update_freq = 1,
-	padding_left = 3,
-	padding_right = 0,
-})
-
--- Double border for calendar using a single item bracket
-sbar.add("bracket", { cal_clock.name, cal_month.name, cal_day_of_week.name, cal_day.name }, {
+sbar.add("bracket", "widgets.calendar.bracket", { cal.name }, {
 	background = {
 		color = colors.tn_black3,
-		height = 34,
 		border_color = colors.tn_blue,
 	},
 })
 
--- Padding item required because of bracket
 sbar.add("item", { position = "right", width = settings.group_paddings })
 
-cal_clock:subscribe({ "forced", "routine", "system_woke" }, function(env)
-	cal_clock:set({ label = os.date("%H:%M") })
+cal:subscribe({ "forced", "routine", "system_woke" }, function(env)
+	local wday = tonumber(os.date("%w")) + 1
+	local date_str = os.date("%m月%d日") .. "(" .. day_of_week_ja[wday] .. ") " .. os.date("%H:%M")
+	cal:set({ label = date_str })
 end)
 
-cal_month:subscribe({ "forced", "routine", "system_woke" }, function(env)
-	cal_month:set({ label = os.date("%b.") })
-end)
-
-cal_day_of_week:subscribe({ "forced", "routine", "system_woke" }, function(env)
-	cal_day_of_week:set({ label = os.date("%a.") })
-end)
-
-cal_day:subscribe({ "forced", "routine", "system_woke" }, function(env)
-	cal_day:set({ label = os.date("%d") })
-end)
-
--- add width
 sbar.add("item", { position = "right", width = 6 })
