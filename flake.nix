ced41feea88c51ab7452eeeeb379185c3913d346
0maru <0maru.dev@gmail.com>
2026-04-17 11:15:21 +0900

    Nix 設定を追加

---
+++ flake.nix
@@ -0,0 +1,27 @@
+{
+  description = "0maru's dotfiles";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = { nixpkgs, home-manager, ... }:
+    let
+      system = "aarch64-darwin";
+      pkgs = nixpkgs.legacyPackages.${system};
+      mkHome = username: home-manager.lib.homeManagerConfiguration {
+        inherit pkgs;
+        modules = [ ./nix/home.nix ];
+        extraSpecialArgs = { inherit username; };
+      };
+    in
+    {
+      homeConfigurations."0maru" = mkHome "0maru";
+      homeConfigurations."3maru" = mkHome "3maru";
+      homeConfigurations."4maru" = mkHome "4maru";
+    };
+}
