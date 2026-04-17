647096931f8bfbaab8691d001184632edef95205
0maru <0maru.dev@gmail.com>
2026-04-17 11:17:07 +0900

    Neovim 設定を追加

---
+++ config/nvim/lua/config/keymaps.lua
@@ -0,0 +1,46 @@
+vim.g.mapleader = " "
+vim.g.maplocalleader = " "
+
+local map = vim.keymap.set
+
+-- プラグイン読込前でも壊れないよう、実行時に smart-splits を解決する
+local function smart_move(direction)
+  return function()
+    require("smart-splits")[direction]()
+  end
+end
+
+-- 保存 / 終了
+map("n", "<Leader>w", "<Cmd>w<CR>", { desc = "Save" })
+map("n", "<Leader>q", "<Cmd>q<CR>", { desc = "Quit" })
+
+-- 折り返し行での移動を改善
+map("n", "j", "v:count == 0 ? 'gj' : 'j'", { expr = true, silent = true })
+map("n", "k", "v:count == 0 ? 'gk' : 'k'", { expr = true, silent = true })
+
+-- smart-splits 経由で Neovim と WezTerm のペイン移動を統一
+map("n", "<C-h>", smart_move("move_cursor_left"), { desc = "Move to left split" })
+map("n", "<C-j>", smart_move("move_cursor_down"), { desc = "Move to lower split" })
+map("n", "<C-k>", smart_move("move_cursor_up"), { desc = "Move to upper split" })
+map("n", "<C-l>", smart_move("move_cursor_right"), { desc = "Move to right split" })
+
+-- バッファの移動
+map("n", "<S-h>", "<Cmd>bprevious<CR>", { desc = "Previous buffer" })
+map("n", "<S-l>", "<Cmd>bnext<CR>", { desc = "Next buffer" })
+map("n", "<Leader>bd", "<Cmd>bdelete<CR>", { desc = "Delete buffer" })
+
+-- 検索ハイライトを消去
+map("n", "<Esc>", "<Cmd>nohlsearch<CR>", { desc = "Clear search highlight" })
+
+-- ビジュアルモードでのインデント改善
+map("v", "<", "<gv")
+map("v", ">", ">gv")
+
+-- 行を上下に移動
+map("v", "J", ":m '>+1<CR>gv=gv", { desc = "Move selection down", silent = true })
+map("v", "K", ":m '<-2<CR>gv=gv", { desc = "Move selection up", silent = true })
+
+-- 診断メッセージの移動
+map("n", "[d", vim.diagnostic.goto_prev, { desc = "Previous diagnostic" })
+map("n", "]d", vim.diagnostic.goto_next, { desc = "Next diagnostic" })
+map("n", "<Leader>ld", vim.diagnostic.open_float, { desc = "Line diagnostics" })
---
+++ config/nvim/lua/config/options.lua
@@ -0,0 +1,50 @@
+local opt = vim.opt
+
+-- 行番号
+opt.number = true
+opt.relativenumber = true
+
+-- インデント
+opt.expandtab = true
+opt.tabstop = 2
+opt.shiftwidth = 2
+opt.smartindent = true
+
+-- 外観
+opt.termguicolors = true
+opt.cursorline = true
+opt.signcolumn = "yes"
+opt.colorcolumn = "100"
+opt.showmode = false -- lualine が表示するので不要
+opt.wrap = false
+
+-- 検索
+opt.ignorecase = true
+opt.smartcase = true
+opt.hlsearch = true
+opt.incsearch = true
+
+-- 動作
+opt.autoread = true
+opt.mouse = "a"
+opt.clipboard = "unnamedplus"
+opt.undofile = true
+opt.swapfile = false
+opt.updatetime = 250
+opt.timeoutlen = 300
+opt.splitright = true
+opt.splitbelow = true
+opt.scrolloff = 8
+opt.sidescrolloff = 8
+
+opt.listchars = { tab = "→ ", trail = "·", eol = "↲", nbsp = "␣", space = "·" }
+opt.list = true
+
+-- 補完
+opt.completeopt = { "menu", "menuone", "noselect" }
+
+vim.filetype.add({
+  extension = {
+    mdx = 'markdown.mdx'
+  }
+})
